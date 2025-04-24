24e7087066b7b6b231d1389d618dff4ee416cbb3
Antoniopereira20003 <2211427@my.ipleiria.pt>
2025-04-24 23:39:57 +0100

    tudo certo

--- Backend/MaleKitty_API/ml_service.py
+++ Backend/MaleKitty_API/ml_service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ccdd6082832172569b7d3d9b0dfb4afb2c146ffd6ef561bbc7ad600ceb8081d
-size 14963
+oid sha256:de6464fd8bbd1bd5272a4424268c82bfcc1f6d4e69b725ae5d4396088035e91c
+size 14939

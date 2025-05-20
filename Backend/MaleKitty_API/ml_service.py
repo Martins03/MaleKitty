bca448feba926ad58acce0785151f79c82952537
Antoniopereira20003 <2211427@my.ipleiria.pt>
2025-05-20 16:16:21 +0100

    melhoras na dnn

--- Backend/MaleKitty_API/ml_service.py
+++ Backend/MaleKitty_API/ml_service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f8ea23f2d082f0312def6c18ca4c5e89a59516cf63052594b484cefcb287cfe
-size 5878
+oid sha256:22ac2295710597a7d8c6d0eb2383d0979a06b3d67c6b8a598e88fd4afb6c6cbf
+size 9667

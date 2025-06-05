59a00f9d1fcbace7917398e45d65f23d44d2c443
Antoniopereira20003 <2211427@my.ipleiria.pt>
2025-06-05 03:17:28 +0100

    não ta perfeito mas da para mostrar ao stor

--- Backend/MaleKitty_API/main.py
+++ Backend/MaleKitty_API/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d5793fda0eb2f174f5a54950cfa3574d5e1f82d96906a7ca1e3171a1de93501
-size 18183
+oid sha256:e91207bf8478fd3e993a80ba96ffd945dfc787f549f3e48a03da3ab21e91d4bf
+size 26215
--- Backend/MaleKitty_API/ml_service.py
+++ Backend/MaleKitty_API/ml_service.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c1b387b2df79d9c3a8e365a636257a4a3585640c5f270688efc3ae14c77c005
-size 39624
+oid sha256:2c228c861ebde3ad5864b45f75fdc629ebfb6b802d99a80c0b1a0011db987158
+size 48408
